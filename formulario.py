23b5c079be0f47ddf0dcd623517bd493a927e00a
IFOA <lilaruscillo@gmail.comgit config --global user.name il-A-ria>
2022-03-11 12:59:50 +0100

    inserisci nuovi file

---
+++ formulario.py
@@ -0,0 +1,22 @@
+
+def area_triang(b,h):
+  areat = b*h/2
+  return areat
+
+#area del cerchio
+def area_cerchio(r):
+  areac = r**2 * 3.14
+  return areac
+
+#perimetro triangolo
+def perimetro_tr(x,y,z):
+  perim = x + y + z
+  return perim
+
+#perimetro triangolo
+def perimetro_cer(r)
+
+#perimetro triangolo
+def perimetro_cer(r):
+  percer = (2 * r) * 3.14
+  return percer
